c0d5c36ec191f0bdfcb46ab47e3542e198bace12
zeeracha <zeeraratcha@gmail.com>
2023-02-17 18:52:54 +0700

    Featured component design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured'
 import "./Home.scss"
 
 const Home = () => {
   return (
     <div className='home'>
-      home
+      <Featured />
     </div>
   )
 }
